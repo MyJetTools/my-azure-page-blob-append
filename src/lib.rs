04f8d9ab50d8723bfafe6b5718d3b01b6007bdcb
Andrey <amigin@gmail.com>
2021-09-23 10:57:03 +0300

    Added Pages Cache with Random Accesss

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
+mod my_date_time;
 mod page_blob_append_cache;
 mod page_blob_append_error;
 mod page_blob_buffer;
+mod page_blob_random_access;
 pub mod page_blob_utils;
+mod pages_cache;
 
 pub use page_blob_append_cache::PageBlobAppendCache;
 
 pub use page_blob_append_error::PageBlobAppendCacheError;
+
+pub use page_blob_random_access::PageBlobRandomAccess;
