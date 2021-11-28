b5f8fb8a8598feb52403ed7612d253055a20fcee
Andrey <amigin@gmail.com>
2021-11-29 00:18:11 +0200

    PageBlobRandomAccess gone to the Other Library

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 mod error;
 mod page_blob_append;
-mod page_blob_random_access;
+
 pub mod page_blob_utils;
-mod pages_cache;
 mod read_write;
 mod settings;
 mod states;
 mod with_retries;
 
+pub use error::PageBlobAppendError;
 pub use page_blob_append::PageBlobAppend;
 
-pub use error::PageBlobAppendError;
-
-pub use page_blob_random_access::PageBlobRandomAccess;
 pub use settings::AppendPageBlobSettings;
 pub use states::{ChangeState, PageBlobAppendCacheState};
